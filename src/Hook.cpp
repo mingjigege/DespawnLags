7dfde7422348d232acdc66f3724b429c38ba4f81
Tsubasa6848 <116721335+Tsubasa6848@users.noreply.github.com>
2023-04-17 13:51:42 +0800

    Update SDK

--- src/Hook.cpp
+++ src/Hook.cpp
@@ -11,8 +11,8 @@
 TInstanceHook(ItemActor*, "?spawnItem@Spawner@@QEAAPEAVItemActor@@AEAVBlockSource@@AEBVItemStack@@PEAVActor@@AEBVVec3@@H@Z", Spawner, BlockSource& a1, ItemStack& a2, Actor* a3, Vec3& a4, int a5) {
     auto it = original(this, a1, a2, a3, a4, a5);
     auto itemtype = it->getItemStack()->getTypeName();
-    if(!Settings::ItemList.count(itemtype)){
-        it->setDespawnTime(Settings::DespawnSeconds);
+    if (!Settings::ItemList.count(itemtype)) {
+        dAccess<int, 1316>(it) = Settings::DespawnSeconds;
     }
     return it;
 }
