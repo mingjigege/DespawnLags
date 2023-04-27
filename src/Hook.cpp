b557952cc70105dcda0638e543dab4faa38adeba
铭记mingji <109053797+mingjigege@users.noreply.github.com>
2023-04-27 11:06:35 +0800

    Update Hook.cpp

--- src/Hook.cpp
+++ src/Hook.cpp
@@ -3,10 +3,17 @@
 #include <mc/ItemStack.hpp>
 #include <mc/ItemActor.hpp>
 
-TClasslessInstanceHook(ItemActor*, "?spawnItem@Spawner@@QEAAPEAVItemActor@@AEAVBlockSource@@AEBVItemStack@@PEAVActor@@AEBVVec3@@H@Z", void* a1, void* a2, void* a3, void* a4, int a5) {
+TClasslessInstanceHook(ItemActor *, "?spawnItem@Spawner@@QEAAPEAVItemActor@@AEAVBlockSource@@AEBVItemStack@@PEAVActor@@AEBVVec3@@H@Z", void *a1, void *a2, void *a3, void *a4, int a5)
+{
     auto it = original(this, a1, a2, a3, a4, a5);
     auto itemtype = it->getItemStack()->getTypeName();
-    if (!Settings::ItemList.count(itemtype)) {
+    if (itemtype == "下界岩")
+    {
+        it->setDespawnTime(10);
+        return it;
+    }
+    if (!Settings::ItemList.count(itemtype))
+    {
         it->setDespawnTime(Settings::DespawnSeconds);
     }
     return it;
